7e3c6b48be8d8e6d38b14041d84362fe246aa92d
Jade Hedrick <jlhedric@edlab-mail.cs.umass.edu>
2015-01-23 10:44:28 -0500

    Second commit

--- hw1.py
+++ hw1.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
+parser = argparser.ArgumentParser()
+
+def dostuff():
+	"""
+	What this does
+	"""
+	print("We'll get back to you on that...")
+
+def usage():
+	"""
+	What this does
+	"""
+	print("Error: /n")
+	print("Usage: hw1.py filename")
+
 def main():	
+	"""
+	What this does
+	"""
+
 	# print("Hello world!")
 	# a = 3
 	# if(a>4):
@@ -8,7 +28,8 @@ def main():
 	# else:
 	# 	print("bar")
 
-	
+
+
 
 if __name__ == '__main__':
 	main()
